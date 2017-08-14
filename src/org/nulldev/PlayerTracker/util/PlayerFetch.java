94766ba1b84b5bfc360a936eeb2fb1e414799d71
NullDev (Shadow) <NL-Dev@yandex.com>
2017-08-14 12:53:06 +0200

    Added code

---
+++ src/org/nulldev/PlayerTracker/util/PlayerFetch.java
@@ -0,0 +1,18 @@
+package org.nulldev.PlayerTracker.util;
+
+////////////////////////////////
+//----------------------------//
+//copyright (c) 2017 NulLDev //
+//----------------------------//
+////////////////////////////////
+
+import java.util.UUID;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+public class PlayerFetch {
+    public static UUID uuid(UUID brokenUUID) { return Bukkit.getOfflinePlayer((UUID)brokenUUID).getUniqueId(); }
+    public static OfflinePlayer offline(UUID playerUUID) { return Bukkit.getOfflinePlayer((UUID)playerUUID); }
+    public static Player player(UUID playerUUID) { return Bukkit.getPlayer((UUID)playerUUID); }
+}
